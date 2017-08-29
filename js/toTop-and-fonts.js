b8c4a630fa80187b47d7649fa4b0d02cac39328f
RoGelione <11>
2017-08-29 17:36:32 +0800

    1111

---
+++ js/toTop-and-fonts.js
@@ -0,0 +1,52 @@
+/*改变字体大小*/			
+			(function (doc, win) {  
+			    var docEl = doc.documentElement,  
+			        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',  
+			        recalc = function () {  
+			            var clientWidth = docEl.clientWidth;  
+			            if (!clientWidth) return;  
+			            if(clientWidth>=640){  
+			                docEl.style.fontSize = '10px';  
+			            }else{  
+			                docEl.style.fontSize = 10 * (clientWidth / 640) + 'px';  
+			            }  
+			        };  
+			  
+			    if (!doc.addEventListener) return;  
+			    win.addEventListener(resizeEvt, recalc, false);  
+			    doc.addEventListener('DOMContentLoaded', recalc, false);  
+			})(document, window);  
+/*返回顶部*/
+			var nst = '';
+			var totop = document.getElementById('toTop');
+			showtop();
+			function showtop(){
+				nst = document.body.scrollTop;
+				if (nst > 300){
+				totop.style.display = 'flex';
+				}else{
+				totop.style.display = 'none';
+				}
+//				console.log(nst)
+			}
+			document.addEventListener('scroll',showtop,false);
+			function smoothscroll(){
+			    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+			    if (currentScroll > 0) {
+			         window.requestAnimationFrame(smoothscroll);
+			         window.scrollTo (0,currentScroll - (currentScroll/5));
+			    }
+			};
+			
+	window.onload=function(){
+		$(".cont-nav").on("click",'span',function(){
+			ToShowNav();
+//			$("#gaimima").load("报名项目.html #gaimima",null,cpassword);/*ie用不了*/
+		});
+		function ToShowNav(){//修改密码可调用此函数
+							$('.overlay,.fixld-box').css("display","block");
+							$('.toClose').on('click','span',function(){
+							    $('.overlay,.fixld-box').css("display","none");
+							});
+							}
+	}
